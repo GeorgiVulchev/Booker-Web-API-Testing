497b7ab6047df5c7b95fe79993d7a4aaf5a15007
Georgi Valchev <georgivvulchev@gmail.com>
2023-07-20 15:10:33 +0300

    Created LoginResponse pojo class

---
+++ Booker-Web-API-Testing/src/main/java/pojo/LoginResponse.java
@@ -0,0 +1,13 @@
+package pojo;
+
+public class LoginResponse {
+	String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
