b66ac699aad70d93f55b80e3e6d2acac126f07fb
Georgi Valchev <georgivvulchev@gmail.com>
2023-07-20 15:12:29 +0300

    Changed the architecture and optimized the steps

--- Booker-Web-API-Testing/src/test/java/stepDefinitions/loginSteps/GenerateToken.java
+++ Booker-Web-API-Testing/src/test/java/stepDefinitions/loginSteps/GenerateToken.java
@@ -1,4 +1,4 @@
-package stepDefinitions;
+package stepDefinitions.loginSteps;
 
 import org.junit.Assert;
 
@@ -16,34 +16,38 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import pojo.LoginRequest;
-
+import pojo.LoginResponse;
+import resources.TestDataBuild;
 import resources.Utils;
 
-public class StepDefinition {
+public class GenerateToken {
 	Utils utils = new Utils();
+	TestDataBuild data = new TestDataBuild();
 	RequestSpecification request;
 	RequestSpecification requestSpec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com")
 			.setContentType(ContentType.JSON).build();
 	Response response;
+	LoginResponse loginResponse;
 	JsonPath js;
-	
+
 	@Given("valid user credentials")
 	public void valid_user_credentials() {
-	    LoginRequest loginRequest = new LoginRequest();
-		loginRequest.setUsername("admin");
-		loginRequest.setPassword("password123");
-		request = given().spec(requestSpec).body(loginRequest);
+		request = given().spec(requestSpec).body(data.loginRequestDetails());
 	}
+
 	@When("user sends a POST request to restful-booker authentication api")
 	public void user_sends_a_post_request_to_restful_booker_authentication_api() {
 		response = request.when().post("/auth");
 	}
+
 	@Then("the API call is successful with status code {int}")
 	public void the_api_call_is_successful_with_status_code(Integer int1) {
 		assertEquals(response.getStatusCode(), 200);
 	}
+
 	@Then("the response body contains an authorization token")
 	public void the_response_body_contains_an_authorization_token() {
-		Assert.assertNotNull(utils.getJsonPath(response, "token"));
+		loginResponse = response.as(LoginResponse.class);
+		Assert.assertNotNull(loginResponse.getToken());
 	}
 }
