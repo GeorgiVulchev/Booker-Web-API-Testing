a095c1e49fb1c28e6fc73ae039f19dfb5db3c44a
Georgi Valchev <georgivvulchev@gmail.com>
2023-09-25 12:24:42 +0300

    Created Hooks class

---
+++ Booker-Web-API-Testing/src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,24 @@
+package stepDefinitions;
+
+import java.io.IOException;
+
+import io.cucumber.java.Before;
+import io.restassured.response.Response;
+import pojo.LoginResponse;
+import stepDefinitions.loginSteps.GenerateToken;
+
+public class Hooks {
+	private static GenerateToken generateToken = new GenerateToken();
+
+	@Before("@CreateBooking")
+	public void beforeScenario() throws IOException {
+		generateToken.valid_user_credentials();
+		generateToken.user_calls_with_http_request("authAPI", "POST");
+	}
+	
+	public static String getToken() {
+		Response response = generateToken.response;
+		String token = response.as(LoginResponse.class).getToken();
+		return token;
+	}
+}
