6b01bd3c4e4236d0616e9d4caca8aec4c528fa72
Georgi Valchev <georgivvulchev@gmail.com>
2023-09-26 10:08:56 +0300

    Created CreateBookingRequestDetails method in TestDataBuild class

--- Booker-Web-API-Testing/src/test/java/resources/TestDataBuild.java
+++ Booker-Web-API-Testing/src/test/java/resources/TestDataBuild.java
@@ -1,14 +1,35 @@
 package resources;
 
+import pojo.BookingDates;
+import pojo.CreateBookingRequest;
 import pojo.LoginRequest;
 
 public class TestDataBuild {
 
 	public LoginRequest loginRequestDetails() {
 		LoginRequest requestDetails = new LoginRequest();
+
 		requestDetails.setUsername("admin");
 		requestDetails.setPassword("password123");
 
 		return requestDetails;
+	}
+
+	public CreateBookingRequest createBookingRequestDetails() {
+		CreateBookingRequest createBookingRequestDetails = new CreateBookingRequest();
+
+		createBookingRequestDetails.setFirstname("Georgi");
+		createBookingRequestDetails.setLastname("Valchev");
+		createBookingRequestDetails.setTotalprice(300);
+		createBookingRequestDetails.setDepositpaid(false);
+		createBookingRequestDetails.setAdditionalneeds("Dinner");
+
+		BookingDates bookingdates = new BookingDates();
+		bookingdates.setCheckin("2023-09-01");
+		bookingdates.setCheckout("2023-09-10");
+
+		createBookingRequestDetails.setBookingdates(bookingdates);
+
+		return createBookingRequestDetails;
 	}
 }
