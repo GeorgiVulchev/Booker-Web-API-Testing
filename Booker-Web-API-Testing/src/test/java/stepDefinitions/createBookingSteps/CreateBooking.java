95429e36cef27437eebb4a143ce7b2c5d1003364
Georgi Valchev <77878464+GeorgiVulchev@users.noreply.github.com>
2023-10-04 15:16:38 +0300

    Updated the steps class for create booking

--- Booker-Web-API-Testing/src/test/java/stepDefinitions/createBookingSteps/CreateBooking.java
+++ Booker-Web-API-Testing/src/test/java/stepDefinitions/createBookingSteps/CreateBooking.java
@@ -1,43 +1,27 @@
 package stepDefinitions.createBookingSteps;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import resources.APIResources;
 import resources.TestDataBuild;
 import resources.Utils;
+import stepDefinitions.commonSteps.CommonSteps;
 
 public class CreateBooking {
-	RequestSpecification request;
-	Utils utils = new Utils();
-	TestDataBuild data = new TestDataBuild();
-	public Response response;
+	private RequestSpecification request;
+	private Utils utils = new Utils();
+	private TestDataBuild data = new TestDataBuild();
+	private CommonSteps commonSteps;
 
-	@Given("test data and a valid authentication token")
-	public void test_data_and_a_valid_authentication_token() throws IOException {
+	public CreateBooking(CommonSteps commonSteps) {
+		this.commonSteps = commonSteps;
+	}
+
+	@Given("test data for creating a booking")
+	public void test_data_for_creating_a_booking() throws IOException {
 		request = given().spec(utils.requestSpecification()).body(data.createBookingRequestDetails());
-	}
-
-	@When("user calls {string} with {string} http request and provide a token")
-	public void user_calls_with_http_request_and_provide_a_token(String resource, String httpMethod) {
-		APIResources resourceAPI = APIResources.valueOf(resource);
-		resourceAPI.getResource();
-
-		if (httpMethod.equalsIgnoreCase("POST"))
-			response = request.when().post(resourceAPI.getResource());
-		else if (httpMethod.equalsIgnoreCase("GET"))
-			response = request.when().get(resourceAPI.getResource());
-	}
-
-	@Then("the API call is successful with status code of {int}")
-	public void the_api_call_is_successful_with_status_code_of(Integer int1) {
-		assertEquals(response.getStatusCode(), 200);
+		commonSteps.setRequest(request);
 	}
 }
