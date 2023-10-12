1428a438ee60574883b41b50c1bb0c838e66b526
Georgi Valchev <77878464+GeorgiVulchev@users.noreply.github.com>
2023-10-12 10:52:40 +0300

    Modified the common steps

--- Booker-Web-API-Testing/src/test/java/stepDefinitions/commonSteps/CommonSteps.java
+++ Booker-Web-API-Testing/src/test/java/stepDefinitions/commonSteps/CommonSteps.java
@@ -6,11 +6,13 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import pojo.CreateBookingResponse;
 import resources.APIResources;
 
 public class CommonSteps {
 	private Response response;
 	private RequestSpecification request;
+	private int id;
 
 	public void setRequest(RequestSpecification request) {
 		this.request = request;
@@ -28,20 +30,28 @@ public class CommonSteps {
 		return response;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	@When("user calls {string} with {string} http request")
 	public void user_calls_with_http_request(String resource, String httpMethod) {
 		APIResources resourceAPI = APIResources.valueOf(resource);
 		resourceAPI.getResource();
 
-		if (httpMethod.equalsIgnoreCase("POST"))
+		if (httpMethod.equalsIgnoreCase("POST")) {
 			response = request.when().post(resourceAPI.getResource());
-		else if (httpMethod.equalsIgnoreCase("GET"))
+			id = response.as(CreateBookingResponse.class).getBookingid();
+		} else if (httpMethod.equalsIgnoreCase("GET"))
 			response = request.when().get(resourceAPI.getResource());
 	}
 
 	@Then("the API call is successful with status code of {int}")
 	public void the_api_call_is_successful_with_status_code_of(int statusCode) {
-		// response = commonSteps.getResponse();
 		assertEquals(response.getStatusCode(), statusCode);
 	}
 }
