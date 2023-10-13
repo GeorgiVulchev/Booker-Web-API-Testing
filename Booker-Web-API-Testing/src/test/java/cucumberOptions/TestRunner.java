9a6601a3eccced4e24a67e00392dae5eaa0abede
Georgi Valchev <77878464+GeorgiVulchev@users.noreply.github.com>
2023-10-13 17:18:26 +0300

    added reporting plugin to the runner class

--- Booker-Web-API-Testing/src/test/java/cucumberOptions/TestRunner.java
+++ Booker-Web-API-Testing/src/test/java/cucumberOptions/TestRunner.java
@@ -6,6 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinitions"}, tags = "@auth")
+@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" }, tags = "@E2E", plugin = { "pretty",
+		"html:target/cucumber-reports/report.html" }, monochrome = true)
+
+//"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 public class TestRunner {
 }
